016338b9aafa10b9fd4778dba2858ac77a400eeb
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2024-09-19 23:51:22 +0500

    fix: map pin issue resolved

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,9 +1,21 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+
+const customIcon = new L.Icon({
+  iconUrl: "/loc_pin.png", // Path to pin.png in the public folder
+  iconSize: [40, 44], // Adjust size to match your needs
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
 const Pin = ({ item }) => {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
       <Popup>
         <div className="popupContainer">
           <img src={item.img} alt="" />
